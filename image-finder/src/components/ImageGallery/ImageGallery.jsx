7bd95faf350dfa94c8f5ffc4ee73eb735a18028f
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-13 18:38:09 +0200

    refactoring via hooks

--- image-finder/src/components/ImageGallery/ImageGallery.jsx
+++ image-finder/src/components/ImageGallery/ImageGallery.jsx
@@ -1,39 +1,38 @@
-import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
 import { GalleryList } from "./ImageGallery.styled";
 
-class ImageGallery extends Component {
-  static propTypes = {
-    request: PropTypes.string.isRequired,
-    page: PropTypes.number.isRequired,
-    updStatePicture: PropTypes.func.isRequired,
-    isShownLoading: PropTypes.func.isRequired,
-    handleError: PropTypes.func.isRequired,
-    onModal: PropTypes.func.isRequired,
-  };
-
-  handleClick = (e) => {
+export default function ImageGallery({
+  request,
+  page,
+  updStatePicture,
+  isShownLoading,
+  handleError,
+  onModal,
+}) {
+  const handleClick = (e) => {
     if (!e.target.dataset.large) return;
-    this.props.onModal(e.target.dataset.large);
+    onModal(e.target.dataset.large);
   };
 
-  render() {
-    const { request, page, updStatePicture, isShownLoading, handleError } =
-      this.props;
-
-    return (
-      <GalleryList onClick={this.handleClick}>
-        <ImageGalleryItem
-          request={request}
-          page={page}
-          updStatePicture={updStatePicture}
-          isShownLoading={isShownLoading}
-          handleError={handleError}
-        />
-      </GalleryList>
-    );
-  }
+  return (
+    <GalleryList onClick={handleClick}>
+      <ImageGalleryItem
+        request={request}
+        page={page}
+        updStatePicture={updStatePicture}
+        isShownLoading={isShownLoading}
+        handleError={handleError}
+      />
+    </GalleryList>
+  );
 }
 
-export default ImageGallery;
+ImageGallery.propTypes = {
+  request: PropTypes.string.isRequired,
+  page: PropTypes.number.isRequired,
+  updStatePicture: PropTypes.func.isRequired,
+  isShownLoading: PropTypes.func.isRequired,
+  handleError: PropTypes.func.isRequired,
+  onModal: PropTypes.func.isRequired,
+};
