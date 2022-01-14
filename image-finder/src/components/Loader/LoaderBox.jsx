9b3cf623b893371bb09818817f6853117bd38cab
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-14 13:47:05 +0200

    refactoring via hooks

--- image-finder/src/components/Loader/LoaderBox.jsx
+++ image-finder/src/components/Loader/LoaderBox.jsx
@@ -1,34 +1,30 @@
-import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { Container, Wrapper } from "./LoaderBox.styled";
 import { theme } from "../../constants/Theme";
 
-export default class LoaderBox extends Component {
-  static propTypes = {
-    loading: PropTypes.bool.isRequired,
-  };
+export default function LoaderBox({ loading }) {
+  const {
+    colors: { accentColor },
+  } = theme;
 
-  render() {
-    const { loading } = this.props;
-    const {
-      colors: { accentColor },
-    } = theme;
-
-    return (
-      <Container>
-        <Wrapper>
-          <Loader
-            type="ThreeDots"
-            color={accentColor}
-            height={100}
-            width={150}
-            timeout={3000}
-            visible={loading}
-          />
-        </Wrapper>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Wrapper>
+        <Loader
+          type="ThreeDots"
+          color={accentColor}
+          height={100}
+          width={150}
+          timeout={3000}
+          visible={loading}
+        />
+      </Wrapper>
+    </Container>
+  );
 }
+
+LoaderBox.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
